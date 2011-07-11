b59966992b5f1a85bc5c858209dbb3712a1a303e
Remy <remyvarma@gmail.com>
2011-07-10 21:49:52 -0700

    Reverting back to old webServer without templates

--- templates.py
+++ templates.py
@@ -30,6 +30,12 @@ _nav = '''<div class="nav">
 			</div>'''
 	
 _footer = '''
-	</div><div class="footer"></div>
+	</div><div class="footer"><br /><div class="center"><form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="93FFC6WDV97QS">
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+</div></div>
 	</body>
 	</html>'''
