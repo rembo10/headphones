a792e8614a428eed659c052d6ecc19b8eecd5bb1
Remy <remyvarma@gmail.com>
2011-08-05 00:22:30 -0700

    Index page styling concept#1

--- data/js/script.js
+++ data/js/script.js
@@ -2,8 +2,11 @@ $(document).ready(function()
 	{
 		$('#artist_table').dataTable(
 			{
+				"aoColumnDefs": [
+					{ "bSOtrable": false, "aTargets": [ 1 ] } ],
 				"bStateSave": true,
 				"iDisplayLength": 50,
 				"sPaginationType": "full_numbers",
+				
 			});
 	});
