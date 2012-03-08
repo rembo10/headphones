68bc290dc44fa88866c24ada1d8cdc5170ee943f
unknown <ramoe40@VIDR95PV9Y.djes.dk>
2012-03-08 23:51:19 +0100

    New images

---
+++ data/js/ActiveLinks.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function () {
+    $("#nav li a").each(function () {
+        var hreflink = $(this).attr("href");
+        if (hreflink == location.href.split("/").pop()) {
+            $(this).parent().addClass("selected");
+        }
+    });
+});
