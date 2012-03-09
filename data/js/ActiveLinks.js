23a067759ba2a8ac73c4fa8ff164293db8b08a92
unknown <ramoe40@VIDR95PV9Y.djes.dk>
2012-03-09 11:46:02 +0100

    CSS Gradient

--- data/js/ActiveLinks.js
+++ data/js/ActiveLinks.js
@@ -1,8 +1,24 @@
-﻿$(document).ready(function () {
+﻿//Mark current active link
+$(document).ready(function () {
     $("#nav li a").each(function () {
         var hreflink = $(this).attr("href");
         if (hreflink == location.href.split("/").pop()) {
             $(this).parent().addClass("selected");
         }
     });
+});
+//Resize #main to fit window size
+$(document).ready(function () {
+	windowWidth = $(window).width();
+	windowHeight = $(window).height();
+	menuWidth = $("#nav").width();
+	$("#main").width(windowWidth - menuWidth - 40);
+});
+
+//On window resize
+$(window).resize(function () {
+	windowWidth = $(window).width();
+	windowHeight = $(window).height();
+	menuWidth = $("#nav").width();
+	$("#main").width(windowWidth - menuWidth - 40);
 });
