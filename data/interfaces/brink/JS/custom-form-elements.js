2999cec0254bd5f9c007291d7eac8f2fc0146d0e
Brinken <rasmuseegmoeller@gmail.com>
2012-03-21 13:32:41 +0100

    Moved files to interface folder named brink

--- data/interfaces/brink/JS/custom-form-elements.js
+++ data/interfaces/brink/JS/custom-form-elements.js
@@ -25,7 +25,7 @@ Visit http://ryanfait.com/ for more information.
 
 var checkboxHeight = "25";
 var radioHeight = "25";
-var selectWidth = "130";
+var selectWidth = "143";
 
 
 /* No need to change anything after this */
