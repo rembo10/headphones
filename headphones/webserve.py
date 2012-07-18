9c8296e547e4af946c34b9499bba1c01be1bc608
rembo10 <rembo10@headphones>
2012-07-18 19:25:39 +0530

    Sort upcoming releases by the soonest first

--- headphones/webserve.py
+++ headphones/webserve.py
@@ -214,7 +214,7 @@ class WebInterface(object):
 
     def upcoming(self):
         myDB = db.DBConnection()
-        upcoming = myDB.select("SELECT * from albums WHERE ReleaseDate > date('now') order by ReleaseDate DESC")
+        upcoming = myDB.select("SELECT * from albums WHERE ReleaseDate > date('now') order by ReleaseDate ASC")
         wanted = myDB.select("SELECT * from albums WHERE Status='Wanted'")
         return serve_template(templatename="upcoming.html", title="Upcoming", upcoming=upcoming, wanted=wanted)
     upcoming.exposed = True
