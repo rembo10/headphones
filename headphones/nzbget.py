6bc97fa6bd59f39ddd5af6d361544ea18681c605
CurlyMoo <development@xbian.org>
2013-02-12 01:23:37 +0100

    Added nzbget support

---
+++ headphones/nzbget.py
@@ -0,0 +1,84 @@
+#  This file is part of Headphones.
+#
+#  Headphones is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  Headphones is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with Headphones.  If not, see <http://www.gnu.org/licenses/>.
+
+#####################################
+## Stolen from Sick-Beard's nzbget.py ##
+#####################################
+
+
+
+import httplib
+import datetime
+
+import headphones
+
+from base64 import standard_b64encode
+import xmlrpclib
+
+#from headphones.providers.generic import GenericProvider
+
+from headphones import logger
+
+def sendNZB(nzb):
+
+    addToTop = False
+    nzbgetXMLrpc = "http://nzbget:%(password)s@%(host)s/xmlrpc"
+
+    if headphones.NZBGET_HOST == None:
+        logger.error(u"No NZBget host found in configuration. Please configure it.")
+        return False
+
+    url = nzbgetXMLrpc % {"host": headphones.NZBGET_HOST, "password": headphones.NZBGET_PASSWORD}
+
+    nzbGetRPC = xmlrpclib.ServerProxy(url)
+    try:
+        if nzbGetRPC.writelog("INFO", "headphones connected to drop of %s any moment now." % (nzb.name + ".nzb")):
+            logger.debug(u"Successful connected to NZBget")
+        else:
+            logger.error(u"Successful connected to NZBget, but unable to send a message" % (nzb.name + ".nzb"))
+
+    except httplib.socket.error:
+        logger.error(u"Please check your NZBget host and port (if it is running). NZBget is not responding to this combination")
+        return False
+
+    except xmlrpclib.ProtocolError, e:
+        if (e.errmsg == "Unauthorized"):
+            logger.error(u"NZBget password is incorrect.")
+        else:
+            logger.error(u"Protocol Error: " + e.errmsg)
+        return False
+
+    # if it's a normal result need to download the NZB content
+    if nzb.resultType == "nzb":
+        genProvider = GenericProvider("")
+        data = genProvider.getURL(nzb.url)
+        if (data == None):
+            return False
+
+    # if we get a raw data result thats even better
+    elif nzb.resultType == "nzbdata":
+        data = nzb.extraInfo[0]
+
+    nzbcontent64 = standard_b64encode(data)
+
+    logger.error(u"Sending NZB to NZBget")
+    logger.debug(u"URL: " + url)
+
+    if nzbGetRPC.append(nzb.name + ".nzb", headphones.NZBGET_CATEGORY, addToTop, nzbcontent64):
+        logger.debug(u"NZB sent to NZBget successfully")
+        return True
+    else:
+        logger.error(u"NZBget could not add %s to the queue" % (nzb.name + ".nzb"))
+        return False
