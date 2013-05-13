fa460ac40a278b702af45c59a161e0ba887a1fe0
rembo10 <rembo10@headphones>
2013-05-13 14:55:04 +0700

    Replace eval() with json.loads() for nzbsrus

--- headphones/searcher.py
+++ headphones/searcher.py
@@ -348,7 +348,7 @@ def searchNZB(albumid=None, new=False, losslessOnly=False):
                 
             if data:
             
-                d = eval(data.replace('null','None'))
+                d = json.loads(data.replace('null','None'))
                 
                 if  d['matches'] <= 0:
                     logger.info(u"No results found from NZBsRus for %s" % term)
