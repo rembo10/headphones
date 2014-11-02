a67d0683cac96b5417c8690cfc100f8e3cece334
Jesse Mullan <jmullan@visi.com>
2014-11-01 19:49:24 -0700

    Suppress a pylint error about something that we are handling

--- headphones/mb.py
+++ headphones/mb.py
@@ -22,7 +22,10 @@ import headphones
 import musicbrainzngs
 
 try:
+    # pylint:disable=E0611
+    # ignore this error because we are catching the ImportError
     from collections import OrderedDict
+    # pylint:enable=E0611
 except ImportError:
     # Python 2.6.x fallback, from libs
     from ordereddict import OrderedDict
