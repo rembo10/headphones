54ee9ed88d5f993698ab85c09d6087e4687a61ab
Bas Stottelaar <basstottelaar@gmail.com>
2014-11-12 23:36:52 +0100

    Fix for new paths

--- contrib/downgrade.sh
+++ contrib/downgrade.sh
@@ -7,7 +7,7 @@ if [ -z "$1" ]; then
 fi
 
 # Version file check
-if [ ! -s "$2/version.lock" ]; then
+if [ ! -s "$1/version.lock" ]; then
     echo "Missing the version.lock file in the data folder, or the file is empty. Did you start Headphones at least once?"
     exit 1
 fi
@@ -19,7 +19,7 @@ if [ ! -x "$(command -v git)" ]; then
 fi
 
 # Display information
-HASH=$(cat $2/version.lock)
+HASH=$(cat $1/version.lock)
 
 echo "This script will try to downgrade Headphones to the last version that started, version $HASH. Make sure you have a backup of your config file and database, just in case!"
 echo "Press enter to continue, or CTRL + C to quit."
