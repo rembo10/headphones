beacc6ed1e7f68f8a0ce52e3f81babb4434d86b1
maxkoryukov <maxkoryukov@yandex.ru>
2016-02-02 16:54:04 +0500

    Prototype for test file

---
+++ headphones/config_test.py
@@ -0,0 +1,9 @@
+import unittest
+from unittest import TestCase
+import mock
+
+from headphones.config import path
+
+class ConfigPathTest(TestCase):
+    def test_path(self):
+        self.assertFalse(True)
