cf33beeddabedadc9403faba45682e20a01932f6
maxkoryukov <maxkoryukov@yandex.ru>
2016-02-03 18:32:02 +0500

    improved testing crutches for python 2.6

--- headphones/unittestcompat.py
+++ headphones/unittestcompat.py
@@ -34,6 +34,12 @@ class TestCase(TC):
     def assertIn(self, *args, **kw):
         return super(TestCase, self).assertIn(*args, **kw)
 
+    def assertIsNone(self, val, msg=None):
+        if not _dummy:
+            return super(TestCase, self).assertIsNone(val, msg)
+        tst = val is None
+        return super(TestCase, self).assertTrue(tst, msg)
+
     def assertIsNotNone(self, val, msg=None):
         if not _dummy:
             return super(TestCase, self).assertIsNotNone(val, msg)
