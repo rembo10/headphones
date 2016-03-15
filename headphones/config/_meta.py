31c2901a84349e9df670afd670b52405fa63a471
maxkoryukov <maxkoryukov@yandex.ru>
2016-03-16 03:22:57 +0500

    * UPG : MetaConfig - loader for meta-info about options, such as ReadOnly or Hidden

---
+++ headphones/config/_meta.py
@@ -0,0 +1,30 @@
+from configobj import ConfigObj
+
+from headphones import logger
+
+class MetaConfig(object):
+    """ Handles metainformation about options """
+
+    def __init__(self, filename):
+        self._filename = filename
+        self._config = ConfigObj(self._filename, encoding='utf-8')
+
+    def apply(self, option):
+        """ Sets up the appropriate meta-configuration for the option 
+
+        Args:
+            - self - self
+            - option - _viewmodel.BaseOption or its ancestor
+        """
+        s = option.model.section
+        k = option.model.inikey
+
+        if (s in self._config) and (k in self._config[s]):
+            mo = self._config[s][k]
+            logger.debug('Set up meta for [{0}][{1}] = [{2}]'.format(s, k, mo))
+            option.visible = True
+            option.readonly = False
+        else:
+            # default meta settings
+            option.visible = True
+            option.readonly = False
