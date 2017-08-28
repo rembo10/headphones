9c2bfcf7d87e8873221daa78ae586a5ab6423b8d
Ade <ade_headphones@yahoo.co.uk>
2017-08-28 20:54:19 +1200

    Scan Artist exception handling

--- headphones/webserve.py
+++ headphones/webserve.py
@@ -353,9 +353,13 @@ class WebInterface(object):
             if not os.path.isdir(artistfolder.encode(headphones.SYS_ENCODING)):
                 logger.debug("Cannot find directory: " + artistfolder)
                 continue
-            threading.Thread(target=librarysync.libraryScan,
-                             kwargs={"dir": artistfolder, "artistScan": True, "ArtistID": ArtistID,
-                                     "ArtistName": artist_name}).start()
+            try:
+                threading.Thread(target=librarysync.libraryScan,
+                                 kwargs={"dir": artistfolder, "artistScan": True, "ArtistID": ArtistID,
+                                         "ArtistName": artist_name}).start()
+            except Exception as e:
+                logger.error('Unable to complete the scan: %s' % e)
+
         raise cherrypy.HTTPRedirect("artistPage?ArtistID=%s" % ArtistID)
 
     @cherrypy.expose
