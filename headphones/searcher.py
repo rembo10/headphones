d2ec43bb196d89ae10074e448a315315d495b746
Mikaël Hautin <3902243+Mikaciu@users.noreply.github.com>
2023-02-05 21:13:41 +0100

    feat(tpb): some TPB proxies changed the HTML structure

--- headphones/searcher.py
+++ headphones/searcher.py
@@ -1761,7 +1761,7 @@ def searchTorrent(album, new=False, losslessOnly=False, albumlength=None,
     # Pirate Bay
     if headphones.CONFIG.PIRATEBAY:
         provider = "The Pirate Bay"
-        tpb_term = term.replace("!", "").replace("'", " ")
+        tpb_term = term.replace("!", "").replace("'", " ").replace(" ", "%20")
 
         # Use proxy if specified
         if headphones.CONFIG.PIRATEBAY_PROXY_URL:
@@ -1793,6 +1793,8 @@ def searchTorrent(album, new=False, losslessOnly=False, albumlength=None,
         # Process content
         if data:
             rows = data.select('table tbody tr')
+            if not rows:
+                rows = data.select('table tr')
 
             if not rows:
                 logger.info("No results found from The Pirate Bay using term: %s" % tpb_term)
