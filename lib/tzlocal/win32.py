a06fb40f50ddfe7dc383eb7afcea0d0cf48678f4
rembo10 <rembo10@users.noreply.github.com>
2024-01-18 14:59:28 +0530

    tzlocal: 1.1.2 -> 5.2

--- lib/tzlocal/win32.py
+++ lib/tzlocal/win32.py
@@ -1,72 +1,78 @@
+import logging
+from datetime import datetime
+
 try:
-    import winreg as winreg
+    import _winreg as winreg
 except ImportError:
     import winreg
 
+try:
+    import zoneinfo  # pragma: no cover
+except ImportError:
+    from backports import zoneinfo  # pragma: no cover
+
+from tzlocal import utils
 from tzlocal.windows_tz import win_tz
-import pytz
 
 _cache_tz = None
+_cache_tz_name = None
+
+log = logging.getLogger("tzlocal")
+
 
 def valuestodict(key):
     """Convert a registry key's values to a dictionary."""
-    dict = {}
+    result = {}
     size = winreg.QueryInfoKey(key)[1]
     for i in range(size):
         data = winreg.EnumValue(key, i)
-        dict[data[0]] = data[1]
-    return dict
+        result[data[0]] = data[1]
+    return result
 
-def get_localzone_name():
+
+def _get_dst_info(tz):
+    # Find the offset for when it doesn't have DST:
+    dst_offset = std_offset = None
+    has_dst = False
+    year = datetime.now().year
+    for dt in (datetime(year, 1, 1), datetime(year, 6, 1)):
+        if tz.dst(dt).total_seconds() == 0.0:
+            # OK, no DST during winter, get this offset
+            std_offset = tz.utcoffset(dt).total_seconds()
+        else:
+            has_dst = True
+
+    return has_dst, std_offset, dst_offset
+
+
+def _get_localzone_name():
     # Windows is special. It has unique time zone names (in several
     # meanings of the word) available, but unfortunately, they can be
     # translated to the language of the operating system, so we need to
     # do a backwards lookup, by going through all time zones and see which
     # one matches.
+    tzenv = utils._tz_name_from_env()
+    if tzenv:
+        return tzenv
+
+    log.debug("Looking up time zone info from registry")
     handle = winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE)
 
     TZLOCALKEYNAME = r"SYSTEM\CurrentControlSet\Control\TimeZoneInformation"
     localtz = winreg.OpenKey(handle, TZLOCALKEYNAME)
     keyvalues = valuestodict(localtz)
     localtz.Close()
-    if 'TimeZoneKeyName' in keyvalues:
-        # Windows 7 (and Vista?)
+
+    if "TimeZoneKeyName" in keyvalues:
+        # Windows 7 and later
 
         # For some reason this returns a string with loads of NUL bytes at
         # least on some systems. I don't know if this is a bug somewhere, I
         # just work around it.
-        tzkeyname = keyvalues['TimeZoneKeyName'].split('\x00', 1)[0]
+        tzkeyname = keyvalues["TimeZoneKeyName"].split("\x00", 1)[0]
     else:
-        # Windows 2000 or XP
-
-        # This is the localized name:
-        tzwin = keyvalues['StandardName']
-
-        # Open the list of timezones to look up the real name:
-        TZKEYNAME = r"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Time Zones"
-        tzkey = winreg.OpenKey(handle, TZKEYNAME)
-
-        # Now, match this value to Time Zone information
-        tzkeyname = None
-        for i in range(winreg.QueryInfoKey(tzkey)[0]):
-            subkey = winreg.EnumKey(tzkey, i)
-            sub = winreg.OpenKey(tzkey, subkey)
-            data = valuestodict(sub)
-            sub.Close()
-            try:
-                if data['Std'] == tzwin:
-                    tzkeyname = subkey
-                    break
-            except KeyError:
-                # This timezone didn't have proper configuration.
-                # Ignore it.
-                pass
-
-        tzkey.Close()
-        handle.Close()
-
-    if tzkeyname is None:
-        raise LookupError('Can not find Windows timezone configuration')
+        # Don't support XP any longer
+        raise LookupError("Can not find Windows timezone configuration")
 
     timezone = win_tz.get(tzkeyname)
     if timezone is None:
@@ -76,18 +82,66 @@ def get_localzone_name():
 
     # Return what we have.
     if timezone is None:
-        raise pytz.UnknownTimeZoneError('Can not find timezone ' + tzkeyname)
+        raise zoneinfo.ZoneInfoNotFoundError(tzkeyname)
+
+    if keyvalues.get("DynamicDaylightTimeDisabled", 0) == 1:
+        # DST is disabled, so don't return the timezone name,
+        # instead return Etc/GMT+offset
+
+        tz = zoneinfo.ZoneInfo(timezone)
+        has_dst, std_offset, dst_offset = _get_dst_info(tz)
+        if not has_dst:
+            # The DST is turned off in the windows configuration,
+            # but this timezone doesn't have DST so it doesn't matter
+            return timezone
+
+        if std_offset is None:
+            raise zoneinfo.ZoneInfoNotFoundError(
+                f"{tzkeyname} claims to not have a non-DST time!?"
+            )
+
+        if std_offset % 3600:
+            # I can't convert this to an hourly offset
+            raise zoneinfo.ZoneInfoNotFoundError(
+                f"tzlocal can't support disabling DST in the {timezone} zone."
+            )
+
+        # This has whole hours as offset, return it as Etc/GMT
+        return f"Etc/GMT{-std_offset//3600:+.0f}"
 
     return timezone
 
-def get_localzone():
+
+def get_localzone_name() -> str:
+    """Get the zoneinfo timezone name that matches the Windows-configured timezone."""
+    global _cache_tz_name
+    if _cache_tz_name is None:
+        _cache_tz_name = _get_localzone_name()
+
+    return _cache_tz_name
+
+
+def get_localzone() -> zoneinfo.ZoneInfo:
     """Returns the zoneinfo-based tzinfo object that matches the Windows-configured timezone."""
+
     global _cache_tz
     if _cache_tz is None:
-        _cache_tz = pytz.timezone(get_localzone_name())
+        _cache_tz = zoneinfo.ZoneInfo(get_localzone_name())
+
+    if not utils._tz_name_from_env():
+        # If the timezone does NOT come from a TZ environment variable,
+        # verify that it's correct. If it's from the environment,
+        # we accept it, this is so you can run tests with different timezones.
+        utils.assert_tz_offset(_cache_tz, error=False)
+
     return _cache_tz
 
-def reload_localzone():
+
+def reload_localzone() -> zoneinfo.ZoneInfo:
     """Reload the cached localzone. You need to call this if the timezone has changed."""
     global _cache_tz
-    _cache_tz = pytz.timezone(get_localzone_name())
+    global _cache_tz_name
+    _cache_tz_name = _get_localzone_name()
+    _cache_tz = zoneinfo.ZoneInfo(_cache_tz_name)
+    utils.assert_tz_offset(_cache_tz, error=False)
+    return _cache_tz
